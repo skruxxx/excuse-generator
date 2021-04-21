aab470395efb2f05a261b2db78e636bf382cd5d9
skruxxx <overlorda@yahoo.com>
2021-04-21 22:10:23 +0000

    excuses

--- src/app.js
+++ src/app.js
@@ -5,7 +5,25 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+function randomgen(array) {
+  return Math.floor(Math.random() * array.length);
+}
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The Dog ", "My Grandma ", "His Turtle ", "My Bird "];
+  let action = ["ate ", "peed ", " crushed ", "broke "];
+  let what = ["my homework ", "the keys ", "the car "];
+  let when = [
+    "before class",
+    "right on time",
+    "when i finished",
+    "during my lunch",
+    "while i was praying"
+  ];
+
+  document.querySelector("#excuse").innerHTML =
+    who[randomgen(who)] +
+    action[randomgen(action)] +
+    what[randomgen(what)] +
+    when[randomgen(when)];
 };
